0f8d34d886fa8defcdc1a6bffdcd7e5adc897264
simon <ouyangyly@gmail.com>
2024-07-03 16:57:25 +0800

    add the report branch for report

--- src/pages/1366x768.vue
+++ src/pages/1366x768.vue
@@ -3,7 +3,7 @@
     <div class="top">
       <div>
         <div class="logo"></div>
-        <h1 class="title">{{ awlStore.title }}</h1>
+        <h1 class="title">园区微电网智慧能源管控平台</h1>
       </div>
       <div class="menus">
         <div class="active">系统总览</div>
@@ -11,35 +11,63 @@
         <div hidden>设备详情</div>
       </div>
     </div>
-    <div class="devices">
-      <w-move :move="false" v-model="xy[0]"><w-acdc @click="routeTo('/watch/acdc', '储能双向变流器')"></w-acdc></w-move>
-      <w-move :move="false" v-model="xy[2]"><w-ac @click="routeTo('/watch/ac', '直驱模拟风机变流器')"></w-ac></w-move>
-      <w-move :move="false" v-model="xy[3]"><w-elec @click="routeTo('/watch/elec', '微电网接入中央测控柜')"></w-elec></w-move>
-      <w-move :move="false" v-model="xy[4]"><w-rlc @click="routeTo('/watch/rlc', 'RLC可编程负载')"></w-rlc></w-move>
-      <w-move :move="false" v-model="xy[5]"><w-ele @click="routeTo('/watch/bms', '电池储能')"></w-ele></w-move>
+    <div class="devicesContainer">
+      <div class="devices">
+        <w-move :move="false" v-model="xy[0]"><w-acdc @click="routeTo('/watch/acdc', '储能双向变流器')"></w-acdc></w-move>
+        <w-move :move="false" v-model="xy[2]"><w-ac @click="routeTo('/watch/ac', '直驱模拟风机变流器')"></w-ac></w-move>
+        <w-move :move="false" v-model="xy[3]"><w-elec @click="routeTo('/watch/elec', '微电网接入中央测控柜')"></w-elec></w-move>
+        <w-move :move="false" v-model="xy[4]"><w-rlc @click="routeTo('/watch/rlc', 'RLC可编程负载')"></w-rlc></w-move>
+        <w-move :move="false" v-model="xy[5]"><w-ele @click="routeTo('/watch/bms', '电池储能')"></w-ele></w-move>
 
-      <!--太阳能-->
-      <w-move :move="false" v-model="xy[6]"><w-sun :running="running.sun" click1="running.sun = !running.sun;"
-          @click="doWind"></w-sun></w-move>
-      <!--风车-->
-      <w-move :move="false" v-model="xy[1]"><w-wind :running="running.wind" click1="running.wind = !running.wind;"
-          @click="doWind"></w-wind></w-move>
-
-
-      <dv-flyline-chart-enhanced :dev="true" :config="config"
-        style="width:100%;height:100%; z-index: 0;  position: absolute;" />
+        <!--太阳能-->
+        <w-move :move="false" v-model="xy[6]"><w-sun :running="running.sun" click1="running.sun = !running.sun;"
+            @click="doWind"></w-sun></w-move>
+        <!--风车-->
+        <w-move :move="false" v-model="xy[1]"><w-wind :running="running.wind" click1="running.wind = !running.wind;"
+            @click="doWind"></w-wind></w-move>
 
 
+        <dv-flyline-chart-enhanced :dev="true" :config="config"
+          style="width:100%;height:100%; z-index: 0;  position: absolute;" />
+
+
+      </div>
+      <div class="swt">
+        <div class="switch">
+          <button @click="toggleSunAnimation" class="glow-on-hover">光伏开关</button>
+
+        </div>
+        <div class="switch">
+          <button @click="toggleWindAnimation" class="glow-on-hover">风电开关</button>
+
+        </div>
+        <div class="switch">
+          <button @click="toggleWindAnimation" class="glow-on-hover">并网开关</button>
+
+        </div>
+      </div>
     </div>
-    <div style="width: 100%; height: 35rem; display: flex; justify-content: center; align-items: center;">
+
+
+    <div
+      style=" width: 100%; height: 35rem; display: flex; justify-content: center; align-items: center; display: none;">
       <w-ls-count :param-titles="['瞬时总有功功率']" :param-ids="[16400]"></w-ls-count>
 
+    </div>
+    <div class="curveChartsContainer" style=" width: 100%; height: 35rem;">
+      <div id="curve">
+        <BMSCharts></BMSCharts>
+        <SunCharts></SunCharts>
+        <WindCharts></WindCharts>
+
+        <PressureCharts></PressureCharts>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, watch, getCurrentInstance, onMounted,onUnmounted } from 'vue';
+import { ref, watch, getCurrentInstance, onMounted, onUnmounted } from 'vue';
 import WAcdc from '../components/energylab/WAcdc.vue';
 import WMove from '../components/WMove.vue';
 import WWind from '../components/energylab/WWind.vue';
@@ -49,8 +77,10 @@ import WRlc from '../components/energylab/WRlc.vue';
 import WEle from '../components/energylab/WEle.vue';
 import WSun from '../components/energylab/WSun.vue';
 import WLsCount from '../components/energylab/WLsCount.vue';
-
-
+import BMSCharts from './chart/BMSCharts.vue';
+import WindCharts from './chart/WindCharts.vue';
+import SunCharts from './chart/SunCharts.vue';
+import PressureCharts from './chart/PressureCharts.vue';
 import { sendAll } from '../WebSocketDevice'
 
 import { doOpen } from '../api/OpensApi'
@@ -71,7 +101,13 @@ const runningChange = () => {
   changeLine(7, running.value.sun);
   changeLine(3, running.value.sun || running.value.wind);
 };
+const toggleSunAnimation = () => {
+  running.value.sun = !running.value.sun;
+};
 
+const toggleWindAnimation = () => {
+  running.value.wind = !running.value.wind;
+};
 //监听状态变化
 watch(running.value, runningChange);
 
@@ -259,7 +295,9 @@ const elecChange = () => {
 }
 watch(() => awlStore.elec, elecChange);
 onMounted(elecChange);
-
+watch(() => awlStore.params["16486"], (newValue, oldValue) => {
+  console.log("Updated temperature:", newValue?.toFixed(2) || 0);
+}, { deep: true, immediate: true });
 //rlc可编程负载变化
 const rlcChange = () => {
   if (awlStore.rlc) {
@@ -306,44 +344,169 @@ window.addEventListener("_timer", () => {
 
 
 const doWind = () => {
-  doOpen(2).then(r=>{
-    if(r.data.ok){
-      ElMessage.success(`发送能源输入模块${awlStore.opens[1]?'打开':'关闭'}命令成功`);
-    }else{
-      ElMessage.error(`发送能源输入模块${awlStore.opens[1]?'打开':'关闭'}命令成功`);
+  doOpen(2).then(r => {
+    if (r.data.ok) {
+      ElMessage.success(`发送能源输入模块${awlStore.opens[1] ? '打开' : '关闭'}命令成功`);
+    } else {
+      ElMessage.error(`发送能源输入模块${awlStore.opens[1] ? '打开' : '关闭'}命令成功`);
       console.error(r);
     }
   });
 }
-watch(() => awlStore.opens[1], ()=>{
-  running.value.sun=awlStore.opens[1];
-  running.value.wind=awlStore.opens[1];
+watch(() => awlStore.opens[1], () => {
+  running.value.sun = awlStore.opens[1];
+  running.value.wind = awlStore.opens[1];
 });
 
 
 
-const resize = ()=>{
-  if (screen.width  < 1366 || screen.height < 768) {
+const resize = () => {
+  if (screen.width < 1366 || screen.height < 768) {
     document.body.style.overflow = "scroll";
-  }else{
+  } else {
     document.body.style.overflow = "hidden";
   }
 }
 
 onMounted(() => {
   resize();
-  window.addEventListener("resize",resize);
+  window.addEventListener("resize", resize);
 });
 onUnmounted(() => {
-  window.removeEventListener("resize",resize);
+  window.removeEventListener("resize", resize);
 });
 </script>
 
-<style scoped>
-.devices {
-  position: relative;
+<style scoped lang="scss">
+.ls-root {
   width: 100%;
-  height: 360px;
-  border-bottom: solid 1px #FFF3;
+  height: 100vh;
+  background-color: #000;
+  background-image: url(../assets/imgs/bg.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  color: #FFF;
+  --shadow-color: #FFF3;
+
+  .top {
+    width: 100%;
+    height: 100px;
+    background-image: url(../assets/imgs/bg-title.png);
+    background-size: cover;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .logo {
+      width: 80px;
+      height: 80px;
+      background-image: url(../assets/imgs/logo-column-2.png);
+      background-size: 100% 100%;
+    }
+
+    .menus {
+      margin-right: 220px;
+    }
+  }
+
+  .devicesContainer {
+    width: 100%;
+    height: 360px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .devices {
+      position: relative;
+      width: 70%;
+      height: 360px;
+    }
+
+    .swt {
+      margin-right: 260px;
+      height: 200px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      font-size: 2rem;
+      font-weight: bold;
+
+      .glow-on-hover {
+        width: 220px;
+        height: 50px;
+        border: none;
+        outline: none;
+        color: #fff;
+        background: #0cf;
+        cursor: pointer;
+        position: relative;
+        z-index: 0;
+        border-radius: 10px;
+
+        &:before {
+          content: '';
+          background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+          position: absolute;
+          top: -2px;
+          left: -2px;
+          background-size: 400%;
+          z-index: -1;
+          filter: blur(5px);
+          width: calc(100% + 4px);
+          height: calc(100% + 4px);
+          animation: glowing 20s linear infinite;
+          opacity: 1; // Always visible, not just on hover
+          transition: opacity .3s ease-in-out;
+          border-radius: 10px;
+        }
+
+        &:active {
+          color: #0cf
+        }
+
+        &:active:after {
+          background: transparent;
+        }
+
+        &:after {
+          z-index: -1;
+          content: '';
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          background: rgb(12, 21, 24);
+          left: 0;
+          top: 0;
+          border-radius: 10px;
+        }
+      }
+
+      @keyframes glowing {
+        0% {
+          background-position: 0 0;
+        }
+
+        50% {
+          background-position: 400% 0;
+        }
+
+        100% {
+          background-position: 0 0;
+        }
+      }
+
+    }
+  }
+
+  .curveChartsContainer {
+    #curve {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+  }
 }
 </style>
---
+++ src/pages/chart/BMSCharts.vue
@@ -0,0 +1,86 @@
+<template>
+    <div ref="chartContainer" style="width: 20%; height: 400px;"></div>
+</template>
+
+<script setup>
+import * as echarts from 'echarts';
+import { onMounted, onUnmounted, ref, watch } from 'vue';
+import { AwlStore } from '../../store';
+
+const awlStore = AwlStore();
+const chartInstance = ref(null);
+const chartContainer = ref(null);
+
+onMounted(() => {
+    chartInstance.value = echarts.init(chartContainer.value);
+    chartInstance.value.setOption({
+        title: {
+            text: 'PV power (W)'
+        },
+        tooltip: {
+            trigger: 'axis',
+            formatter: (params) => {
+                params = params[0];
+                const date = new Date();
+                return date.toLocaleTimeString('zh-CN', { minute: '2-digit', second: '2-digit' }) + ' : ' + params.value[1].toFixed(2) + ' ℃';
+            },
+            axisPointer: {
+                animation: false
+            }
+        },
+        xAxis: {
+            type: 'time',
+            splitLine: {
+                show: false
+            },
+            axisLabel: {
+                formatter: (value) => {
+                    // 将时间戳格式化为仅显示分钟和秒
+                    return echarts.format.formatTime('mm', value);
+                }
+            }
+        },
+        yAxis: {
+            type: 'value',
+            boundaryGap: [0, '100%'],
+            splitLine: {
+                show: false
+            }
+        },
+        series: [{
+            name: 'Temperature',
+            type: 'line',
+            showSymbol: false,
+            hoverAnimation: false,
+            data: []
+        }]
+    });
+
+    // 监视特定参数的变化，并更新图表数据
+    watch(() => awlStore.params["16486"], (newValue) => {
+        console.log(awlStore.params)
+        updateChartData(newValue * 2 + 1500);
+    }, { immediate: true });
+});
+
+onUnmounted(() => {
+    if (chartInstance.value) {
+        chartInstance.value.dispose();
+    }
+});
+
+function updateChartData(newValue) {
+    const now = new Date();
+    const dataPoint = { value: [now, newValue] };
+
+    const option = chartInstance.value.getOption();
+    option.series[0].data.push(dataPoint);
+
+    // 保持图表数据点数量，例如最近100个数据点
+    if (option.series[0].data.length > 100) {
+        option.series[0].data.shift();
+    }
+
+    chartInstance.value.setOption(option, { notMerge: true });
+}
+</script>
